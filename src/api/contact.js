f59703b58a272295dc388e77680506aab2218489
Alexandre <alexandre.cariou@outlook.com>
2018-01-09 12:00:46 +0100

    fix promise contact.search

--- src/api/contact.js
+++ src/api/contact.js
@@ -202,9 +202,14 @@ function search(client, msg) {
                     connected: false,
                     contact: false,
                   };
-                  userManagement.connected(contact.login).then(() => { tmp.connected = true; });
-                  isAContact(data.id, contact.id).then(() => { tmp.contact = true; });
-                  userContacts.push(tmp);
+                  userManagement
+                    .connected(contact.login)
+                    .then(() => { tmp.connected = true; })
+                    .finally(() => {
+                      isAContact(data.id, contact.id)
+                        .then(() => { tmp.contact = true; })
+                        .finally(() => { userContacts.push(tmp); });
+                    });
                 }
                 i += 1;
                 if (i === len) {
