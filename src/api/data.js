92f1bc161a4773fc77245aeb4bb6ad61ce76cd49
Frederic <frederic.lavrut@epitech.eu>
2018-01-25 19:24:38 +0100

    api/data unset binary for stdin Dockerfile global attributes all files as binary

--- src/api/data.js
+++ src/api/data.js
@@ -19,7 +19,7 @@ const prReadFile = url =>
 const prExecFileStdin = (url: string, args: any, opt?: any, stdin: any) =>
   new Promise((res) => {
     const c = execFile(url, args, opt, (error, stdout, stderr) => res({ error, stdout, stderr }));
-    c.stdin.end(stdin, 'binary');
+    c.stdin.end(stdin);
   });
 
 type Client = {
